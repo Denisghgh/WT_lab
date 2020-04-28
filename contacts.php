b4c3e4326ce3fd1cdf42bc113219e67f0903899b
Denis March <denismarhenko123@gmail.com>
2020-04-28 19:01:31 +0300

    Added 2 lab_updated

---
+++ contacts.php
@@ -0,0 +1,97 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8 " />
+    <title>Инструменты</title>
+    <link href="css/style.css" rel="stylesheet">
+    <link href="css/contacts.css" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Oswald:300,400,700&display=swap" rel="stylesheet">
+	<meta content="width=device-width, initial-scale=1" name="viewport" />
+</head>
+<body>
+    <div class="contacts-wrapper">
+        <div class="header">
+            <nav>
+				<?php include "lab2_common.php";?>
+			</nav>
+            <div class="separator"> </div>
+        	<article class="image-box">
+        		<img src="images1/1.png" alt=""/>
+        		<div class="mask"></div>
+        		<div class="info-box">
+        			<h2><a href="index.html">взрослые</a></h2>
+        			<p>Здесь можно найти взрослые носки</p>
+        		</div>
+        	</article>
+        	<article class="image-box">
+        		<img src="images1/2.png" alt=""/>
+        		<div class="mask"></div>
+        		<div class="info-box">
+        			<h2><a href="index.html">детские</a></h2>
+        			<p>Здесь можно найти детские носки</p>
+        		</div>
+        	</article>
+        	<article class="image-box">
+        		<img src="images1/3.png" alt=""/>
+        		<div class="mask"></div>
+        		<div class="info-box">
+        			<h2><a href="index.html">подростковые</a></h2>
+        			<p>Здесь можно найти подростковые носки</p>
+        		</div>
+        	</article>
+        </div>
+
+        <div class="contacts-list">
+            <div class = "contacts-list-name">
+                Контакты
+            </div>
+            <div class="contact">
+                <div class="ico-wrap"> </div>
+                <div class="meanous-andscoev">
+                    <h4 class="aio-icon-title">Юридический адрес:</h4>
+                    <div class="aio-icon-description">
+                      Частное Предприятие "АдвантМетПласт"
+                     223028, Минский р-н, Ждановичский с/с, аг. Ждановичи,
+                     ул. Заречная, 1В-24
+                        <br>дом 14, литер D, Кабинет # 31
+                    </div>
+                </div>
+                <span class="kesednsoled"></span>
+            </div>
+            <div class="contact">
+                  <div class="ico-wrap"> </div>
+                <div class="meanous-andscoev">
+                    <h4 class="aio-icon-title">График работы складов и офисов компании:</h4>
+                    <div class="aio-icon-description">
+                        пн.-сб. с 10:00 до 21:00, с перерывом на обед
+                        <br>Без выходных
+                    </div>
+                </div>
+                <span class="kesednsoled"></span>
+            </div>
+            <div class="contact">
+                  <div class="ico-wrap"> </div>
+                <div class="meanous-andscoev">
+                    <h4 class="aio-icon-title">Электронная почта:</h4>
+                    <div class="aio-icon-description">
+                        denisgh@tut.by
+                        <br>NOSOK@mail.ru
+                    </div>
+                </div>
+                <span class="kesednsoled"></span>
+            </div>
+            <div class="contact">
+                  <div class="ico-wrap"> </div>
+                <div class="meanous-andscoev">
+                    <h4 class="aio-icon-title">Связь по телефону:</h4>
+                    <div class="aio-icon-description">
+                        8 (423) 031-53-78
+                        <br>8 (723) 904-52-7
+                    </div>
+                </div>
+                <span class="kesednsoled"></span>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
